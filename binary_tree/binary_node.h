870b2b2850fd9ede4da9f207f5b5224080a882d3
chenlu <chenlu888@126.com>
2024-08-19 11:23:07 +0800

    初步确定二叉树的接口

--- binary_tree/binary_node.h
+++ binary_tree/binary_node.h
@@ -1,12 +1,10 @@
 #ifndef BINARY_NODE_H
 #define BINARY_NODE_H
 
-#if defined(DSA_REDBLACK)  // Red-Black Tree
-#define stature(p) \
-    ((p) ? (p)->height : 0)  // initial height is 0, else recursive
+#if defined(DSA_REDBLACK)                   // Red-Black Tree
+#define stature(p) ((p) ? (p)->height : 0)  // initial height is 0, else recursive
 #else
-#define stature(p) \
-    ((int)((p) ? (p)->height : -1))  // initial height is -1, else recursive
+#define stature(p) ((int)((p) ? (p)->height : -1))  // initial height is -1, else recursive
 #endif
 
 typedef enum { RB_RED, RB_BLACK } RBColor;
@@ -22,33 +20,30 @@ class BinaryNode {
           null_path_length_(1),
           color_(RB_RED) {}
 
-    BinaryNode(T e, BinaryNode<T>* parent = nullptr,
-               BinaryNode<T>* left_child = nullptr,
-               BinaryNode<T>* right_child = nullptr, int height = 0, int npl = 1,
-               RBColor color = RB_RED)
+    BinaryNode(T e, BinaryNode<T>* parent = nullptr, BinaryNode<T>* left_child = nullptr,
+               BinaryNode<T>* right_child = nullptr, int height = 0, int npl = 1, RBColor color = RB_RED)
         : data_(e),
           parent_(parent),
           left_child_(left_child),
           right_child_(right_child),
           height_(height),
-          nullptr_path_length_(npl),
+          null_path_length_(npl),
           color_(color) {}
 
-    bool operator<(BinaryNode<T>* const &bn) { return data_ < bn.data() }
-    bool operator==(BinaryNode<T>* const &bn){return data_ == bn.data()}
+    bool operator<(BinaryNode<T>* const& bn) { return data_ < bn.data() }
+    bool operator==(BinaryNode<T>* const& bn){return data_ == bn.data()}
 
     T data(){return data_};
     void set_data_(T data){data_ = data};
 
     BinaryNode<T>* parent(){return parent_};
-    void set_parent(BinaryNode<T> *parent){parent_ = parent};
+    void set_parent(BinaryNode<T>* parent){parent_ = parent};
 
     BinaryNode<T>* left_child(){return left_child_};
     void set_left_child(BinaryNode<T>* left_child){left_child_ = left_child};
 
     BinaryNode<T>* right_child(){return right_child_};
-    void set_right_child(BinaryNode<T>* right_child){
-        right_child_ = right_child};
+    void set_right_child(BinaryNode<T>* right_child){right_child_ = right_child};
 
     size_t height(){return height_};
     void set_height(size_t height){height_ = height};
@@ -61,7 +56,7 @@ class BinaryNode {
 
    private:
     T data_;
-    BinaryNode<T> *parent_, *left_child_, *right_child_;
+    BinaryNode<T>*parent_, *left_child_, *right_child_;
     size_t height_;
     size_t null_path_length_;  // nullptr Path Length, left heap
     RBColor color_;            // used for red black tree
--- binary_tree/binary_tree.h
+++ binary_tree/binary_tree.h
@@ -6,9 +6,50 @@
 template <typename T>
 class BinaryTree {
    public:
-    BinaryTree(){};
+    BinaryTree() : size_(0), root_(nullptr) {}
+    ~BinaryTree() {
+        if (0 < size_) deleteChildTree(root_);
+    }
 
-   private:
+    size_t size() const { return size_; }
+    bool empty() const { return root_ != nullptr; }
+
+    size_t deleteChildTree(BinaryNode<T>*) {
+        // parent pointer need to point nullptr
+        // update ancestors' height
+        // delete sub tree x
+        // return size
+        return 0;
+    }
+
+    BinaryTree<T>* datachChildTree(BinaryNode<T>*) { return nullptr; }
+
+    BinaryNode<T>* root(){return root_};
+    void set_root(BinaryNode<T>* root){root_ = root};
+
+    BinaryNode<T>* insertLeftNode(T e, BinaryNode<T>* parent) {
+        BinaryNode<T>* left_child = new BinaryNode<T>(e) parent->set_left_child(left_child);
+        updateHeightAbove(left_child);
+        ++size_;
+        return parent->left_child();
+    }
+
+    BinaryNode<T>* insertRightNode(T, BinaryNode<T>* parent) {
+        BinaryNode<T>* right_child = new BinaryNode<T>(e) parent->set_right_child(right_child);
+        updateHeightAbove(right_child);
+        ++size_;
+        return parent->right_child();
+    }
+
+    BinaryNode<T>* insertLeftTree(BinaryTree<T>* left_tree, BinaryNode<T>* parent) {}
+
+    BinaryNode<T>* insertRightTree(BinaryTree<T>* right_tree, BinaryNode<T>* parent) {}
+
+   protected:
+    // update height of tree node x
+    virtual size_t updateHeight(BinaryNode<T>* x) { return 0; }
+    // update height of tree node x and its ancestors
+    void updateHeightAbove(BinaryNode<T>* x) {}
     size_t size_;
     BinaryNode<T>* root_;
 };
