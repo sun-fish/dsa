c3cb5c40bf77525d9b75f141c592e921fd73c9f2
chenlu <chenlu888@126.com>
2024-08-22 15:47:10 +0800

    新增图工程

---
+++ graph/compile_test.cpp
@@ -0,0 +1,7 @@
+#include <assert.h>
+
+#include "utility.h"
+
+bool graph_test() {
+    return true;
+}
---
+++ graph/utility.h
@@ -0,0 +1,6 @@
+#ifndef GRAPH_UTILITY
+#define GRAPH_UTILITY
+
+#include <assert.h>
+
+#endif  // !GRAPH_UTILITY
